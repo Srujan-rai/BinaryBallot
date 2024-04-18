725482ccc253c00f05b42d495fb12f19880536be
Srujan-rai <raisrujan@gmail.com>
2024-04-18 22:47:54 +0530

    code push

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ import serial
 
 app=Flask(__name__)
 
+ser=serial.Serial('COM3',9600)
+while True:
+    ser.write("yes")
+    data = ser.readline().decode().strip()
+    print("Data from Arduino:", data)
+    ser.write("yes")
+    
 
+ser.close()
 
+@app.route('/')
+def home():
+    return "<h1>hello</h1>"
 
 
 
